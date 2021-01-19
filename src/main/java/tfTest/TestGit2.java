91c8a363319e261a967880c36d852a9c1f3a19d1
abirzhan <er.birzhan@gmail.com>
2021-01-19 12:59:04 -0500

    3rd commit

---
+++ src/main/java/tfTest/TestGit2.java
@@ -0,0 +1,14 @@
+package tfTest;
+
+public class TestGit2 {
+
+	public static void main(String[] args) {
+		printMe();
+		
+	}
+	public static void printMe() {
+		System.out.println("Test Git");
+	}
+
+
+}
